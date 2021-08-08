6539ef68b788d881ae70961888d540658a2ef101
Pavel Fomin <webdev.fominpa@gmail.com>
2021-08-09 00:37:26 +0400

    fix _section-title

--- blocks/_section-title/_section-title.vue.js
+++ blocks/_section-title/_section-title.vue.js
@@ -6,7 +6,9 @@ export default {
 		<div class="section-title__hr"></div>
 		<div :class="iconClass" class="section-title__icon"><i :class="faIconClass"></i></div>
 		<div class="section-title__hr"></div>
-		<h2 class="section-title__title">{{ title }}</h2>
-		<p v-if="description" class="section-title__description">{{ description }}</p>
+		<div class="section-title__text">
+			<h2 class="section-title__title"><span v-html='title'></span></h2>
+			<p v-if="description" class="section-title__description">{{ description }}</p>
+		</div>
 	</div>`
 }
